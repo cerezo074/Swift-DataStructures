94d8d7e7db0f98031e5c3ecd192e1c6e30a69c3f
Eli Pacheco Hoyos <eli@balancemy.money>
2019-07-03 07:09:08 -0500

    Implement doubly linked list

--- Chapter 5 - Queues/Queue.playground/Pages/QueueLinkedList.xcplaygroundpage/Sources/DoublyLinkedList.swift
+++ Chapter 5 - Queues/Queue.playground/Pages/QueueLinkedList.xcplaygroundpage/Sources/DoublyLinkedList.swift
@@ -104,3 +104,35 @@ extension DoublyLinkedList: Sequence {
     return LinkedListIterator(node: head)
   }
 }
+
+public struct QueueLinkedList<T>: Queue {
+    private let list = DoublyLinkedList<T>()
+    
+    public var isEmpty: Bool {
+        return list.isEmpty
+    }
+    
+    public var peek: T? {
+        return list.first?.value
+    }
+    
+    public init() {}
+    
+    public mutating func enqueue(_ element: T) -> Bool {
+        list.append(element)
+        return true
+    }
+    
+    public mutating func dequeue() -> T? {
+        guard !isEmpty, let head = list.first else { return nil }
+        return list.remove(head)
+    }
+}
+
+extension QueueLinkedList: CustomStringConvertible {
+    
+    public var description: String {
+        return list.description
+    }
+    
+}
