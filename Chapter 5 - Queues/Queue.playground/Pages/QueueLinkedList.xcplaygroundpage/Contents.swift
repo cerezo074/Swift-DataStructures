4c4290503621346feca9cdaf76d027be5bc4da2e
Eli Pacheco Hoyos <eli@balancemy.money>
2019-07-22 06:17:23 -0500

    Implement ring buffer

--- Chapter 5 - Queues/Queue.playground/Pages/QueueLinkedList.xcplaygroundpage/Contents.swift
+++ Chapter 5 - Queues/Queue.playground/Pages/QueueLinkedList.xcplaygroundpage/Contents.swift
@@ -1,3 +1,34 @@
+public struct QueueLinkedList<T>: Queue {
+    private let list = DoublyLinkedList<T>()
+    
+    public var isEmpty: Bool {
+        return list.isEmpty
+    }
+    
+    public var peek: T? {
+        return list.first?.value
+    }
+    
+    public init() {}
+    
+    public mutating func enqueue(_ element: T) -> Bool {
+        list.append(element)
+        return true
+    }
+    
+    public mutating func dequeue() -> T? {
+        guard !isEmpty, let head = list.first else { return nil }
+        return list.remove(head)
+    }
+}
+
+extension QueueLinkedList: CustomStringConvertible {
+    
+    public var description: String {
+        return list.description
+    }
+    
+}
 
 var queue = QueueLinkedList<String>()
 queue.enqueue("Ray")
