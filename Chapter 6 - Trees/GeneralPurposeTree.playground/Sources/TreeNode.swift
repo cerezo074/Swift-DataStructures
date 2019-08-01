b5cdf44f4cc4c8d88ddc5afdaf2ff376b37412c3
Eli Pacheco Hoyos <eli@balancemy.money>
2019-07-31 23:27:37 -0500

    Add tree, add For eachs travels

---
+++ Chapter 6 - Trees/GeneralPurposeTree.playground/Sources/TreeNode.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+public class TreeNode<T> {
+    public var value: T
+    public var children: [TreeNode] = []
+    
+    public init(_ value: T) {
+        self.value = value
+    }
+    
+    public func add(_ child: TreeNode) {
+        children.append(child)
+    }
+}
+
+extension TreeNode {
+    
+    public func forEachDepthFirst(visit: (TreeNode) -> Void) {
+        visit(self)
+        children.forEach {
+            $0.forEachDepthFirst(visit: visit)
+        }
+    }
+    
+    public func forEachLevelOrder(visit: (TreeNode) -> Void) {
+        visit(self)
+        var queue = Queue<TreeNode>()
+        children.forEach { queue.enqueue($0) }
+        
+        while let child = queue.dequeue() {
+            visit(child)
+            child.children.forEach { queue.enqueue($0) }
+        }
+    }
+    
+}
