c71451eea3cf8221e47d240143ca24d7e4bbccff
Eli Pacheco Hoyos <eli@balancemy.money>
2019-09-18 06:49:21 -0500

    Balance delete operation

--- Chapter 16 - AVL Trees/Starter.playground/Sources/AVLTree.swift
+++ Chapter 16 - AVL Trees/Starter.playground/Sources/AVLTree.swift
@@ -151,6 +151,10 @@ extension AVLTree {
         } else {
             node.rightChild = remove(node: node.rightChild, value: value)
         }
-        return node
+
+        let balancedNode = balanced(node)
+        balancedNode.height = max(balancedNode.leftHeight, balancedNode.rightHeight) + 1
+
+        return balancedNode
     }
 }
