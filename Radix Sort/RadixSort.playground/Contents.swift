ef29eee7aff11002058a29f4f26eaed19583203b
Eli Pacheco <eph_074@gmail.com>
2019-11-13 08:55:04 -0500

    Minor change

--- Radix Sort/RadixSort.playground/Contents.swift
+++ Radix Sort/RadixSort.playground/Contents.swift
@@ -25,9 +25,17 @@ extension Array where Element == Int {
             
             digits *= base
             self = buckets.flatMap { $0 }
+            print(buckets)
         }
     }
     
+}
+
+example(of: "radix sort") {
+  var array = [88, 410, 1772, 20]
+  print("Original array: \(array)")
+  array.radixSort()
+  print("Radix sorted: \(array)")
 }
 
 
