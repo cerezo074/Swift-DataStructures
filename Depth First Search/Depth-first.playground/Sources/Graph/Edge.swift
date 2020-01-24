8d3a4448904bd21a535b264da41fd168ce08a5a5
Eli Pacheco <eph_074@gmail.com>
2020-01-24 08:52:06 -0500

    Minor change

--- Depth First Search/Depth-first.playground/Sources/Graph/Edge.swift
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2018 Razeware LLC
-// For full license & permission details, see LICENSE.markdown.
-
-public struct Edge<T> {
-  
-  public let source: Vertex<T>
-  public let destination: Vertex<T>
-  public let weight: Double?
-}
