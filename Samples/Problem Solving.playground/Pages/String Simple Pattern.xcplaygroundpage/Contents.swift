7a4672f9c8d46e77678284d9d7fa4f39edd2a03a
Eli Pacheco <eph_074@gmail.com>
2020-02-17 22:49:35 -0500

    implement string simple pattern

---
+++ Samples/Problem Solving.playground/Pages/String Simple Pattern.xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func match(in words: String, _ pattern: String) -> Bool {
+    let wordsCollection = words.split(separator: " ").map { String($0) }
+    let patternCollection = Array(pattern).map { String($0) }
+    let wordsIdentifiers = transform(collection: wordsCollection)
+    let patternIdentifiers = transform(collection: patternCollection)
+    
+    guard wordsIdentifiers.count == patternIdentifiers.count else { return false }
+    
+    return wordsIdentifiers.intersection(patternIdentifiers).count == wordsIdentifiers.count
+}
+
+func transform(collection: [String]) -> Set<String> {
+    var indexIdentifiers: Set<String> = []
+    var indexMap: [String: String] = [:]
+    
+    for (index, word) in collection.enumerated() {
+        if let wordIndexes = indexMap[word] {
+            indexMap[word] = wordIndexes + "," + "\(index)"
+        } else {
+            indexMap[word] = "\(index)"
+        }
+    }
+    
+    for (_, value) in indexMap {
+        indexIdentifiers.insert(value)
+    }
+    
+    return indexIdentifiers
+}
+
+let pattern = "abca"
+let words = "cat dog rabbit cat"
+print(match(in: words, pattern))
